e52b195d0f8ec385c8789164db041a67b623a40a
GamalAhmed29 <gimyahmed028@gmail.com>
2024-07-07 14:08:34 +0300

    initial commit

--- js/details.js
+++ js/details.js
@@ -63,12 +63,16 @@ class GameDetails {
         <p><strong class="gameDetails">Platform:</strong> <span id="gamePlatform" class="bg-info">${gameDetails.platform}</span></p>
         <p><strong class="gameDetails">Status:</strong> <span id="gamePublisher" class="bg-info">${gameDetails.status}</span></p>
         <div class="gameDetails" id="gameDescription">${gameDetails.description}</div>
-         <button class="btn btn-outline-secondary buttonn" data-game-id="${gameDetails.id}">Veiw</button>
+         <button class="btn btn-outline-secondary buttonn" data-game-url="${gameDetails.game_url}">View</button>
       </div>
       
     `;
+    
 
     document.getElementById("row").innerHTML = content;
+     document.querySelector(".buttonn").addEventListener("click", function () {
+       window.location.href = this.getAttribute("data-game-url");
+     });
   }
 }
 const gameDetails = new GameDetails();
